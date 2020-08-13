68d8cdcff0cb6d7f46962f75868d91c62cd99253
Revanth Rokkam <revanthrokkam1@gmail.com>
2020-08-13 10:28:03 +0530

    Errors checked and added new test cases

--- client_test.py
+++ client_test.py
@@ -9,7 +9,7 @@ class ClientTest(unittest.TestCase):
     ]
     """ ------------ Add the assertion below ------------ """
     for quote in quotes:
-        self.assertEqual(getDataPoint(quote),(quote['stock'],float(quote['top_bid']['price']),float(quote['top_ask']['price'],(bid_price+ask_price)/2))
+        self.assertEqual(getDataPoint(quote),(quote['stock'],float(quote['top_bid']['price']),float(quote['top_ask']['price']),(float(quote['top_bid']['price'])+float(quote['top_ask']['price']))/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -18,14 +18,21 @@ class ClientTest(unittest.TestCase):
     ]
     """ ------------ Add the assertion below ------------ """
     for quote in quotes:
-        self.assertEqual(getDataPoint(quote),(quote['stock'],float(quote['top_bid']['price']),float(quote['top_ask']['price'],(bid_price+ask_price)/2))
+        self.assertEqual(getDataPoint(quote),(quote['stock'],float(quote['top_bid']['price']),float(quote['top_ask']['price']),(float(quote['top_bid']['price'])+float(quote['top_ask']['price']))/2))
 
   """ ------------ Add more unit tests ------------ """
   def test_getRatio_price_b_equal_to_zero(self):
-      self.assertEqual(getRatio(2,0),None)
+      self.assertIsNone(getRatio(2,0))
 
-  def test_getRatio_findRatio(self):
-      self.assertEqual(getRatio(87.97,85.93),1.0237402536948679)  
+  def test_getRatio_greaterThanOne(self):
+      self.assertGreater(getRatio(87.97,85.93),1)
+
+  def test_getRatio_lessThanOne(self):
+      self.assertLess(getRatio(85.93,87.97),1)
+
+  def test_getRatio_equalToOne(self):
+      self.assertEqual(getRatio(85.93,85.93),1)
+
 
 if __name__ == '__main__':
     unittest.main()
